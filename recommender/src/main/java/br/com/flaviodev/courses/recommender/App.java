84de4ba9088f9dd7352d045be186e00517c3af0d
flavio <fdsdev@gmail.com>
2018-01-24 00:10:38 -0200

    calculating margin of error

--- recommender/src/main/java/br/com/flaviodev/courses/recommender/App.java
+++ recommender/src/main/java/br/com/flaviodev/courses/recommender/App.java
@@ -6,13 +6,8 @@ import java.util.List;
 
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
-import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
-import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
-import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
-import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
-import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 /**
  * Hello world!
@@ -23,10 +18,8 @@ public class App {
 		File file = new File("src/main/resources/dados.csv");
 		FileDataModel model = new FileDataModel(file);
 
-		UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
-		UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
-		UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
-
+		UserBasedRecommender recommender = (UserBasedRecommender) new ProductRecommender().buildRecommender(model);
+	
 		List<RecommendedItem> recommendations = recommender.recommend(2, 3);
 		for (RecommendedItem recommendation : recommendations) {
 			System.out.println(recommendation);
---
+++ recommender/src/main/java/br/com/flaviodev/courses/recommender/Evaluator.java
@@ -0,0 +1,30 @@
+package br.com.flaviodev.courses.recommender;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.common.RandomUtils;
+
+public class Evaluator {
+
+	public static void main(String[] args) throws IOException, TasteException {
+		
+		RandomUtils.useTestSeed();
+		
+		File file = new File("src/main/resources/dados.csv");
+		FileDataModel model = new FileDataModel(file);
+		
+		RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
+		
+		RecommenderBuilder builder = new ProductRecommender();
+		// 0.9 trainning - 1.0 testing
+		double marginOfError = evaluator.evaluate(builder, null, model, 0.9, 1.0); 
+		System.out.println(marginOfError);
+	}
+	
+}
